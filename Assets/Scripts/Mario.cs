0d1e28c1cf560d8b138fddb39cf75c889e517037
redbdb <gustavoasamaral@gmail.com>
2025-03-22 12:28:55 -0300

    movimentação quase completa

---
+++ Assets/Scripts/Mario.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class Mario : MonoBehaviour
+{
+    public float velocidade;
+    public float ForcaPulo;
+    public Rigidbody2D corpo;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Mover();
+        Pular();
+    }
+
+    void Mover()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"),0f,0f);
+        transform.position += movement * Time.deltaTime * velocidade;
+    }
+
+    void Pular()
+    {
+        if(Input.GetButtonDown("Jump"))
+            corpo.AddForce(new Vector2(0f, ForcaPulo), ForceMode2D.Impulse);
+    }
+}
