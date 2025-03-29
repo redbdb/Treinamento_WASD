cc53210e79eac753ff010615d9920b9be24fa171
redbdb <gustavoasamaral@gmail.com>
2025-03-29 20:17:26 -0300

    goomba andando(teoricamente o koopa tbm)

---
+++ Assets/Scripts/Goomba.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Goomba : MonoBehaviour
+{
+    public int velocidade = 5;
+    public Rigidbody2D corpo;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        velocidade = -velocidade;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        corpo.linearVelocity = new Vector2(velocidade, corpo.linearVelocity.y);
+    }
+}
