f5c2f99f7041f880116947b77f5d70b6286116f4
redbdb <gustavoasamaral@gmail.com>
2025-03-30 15:56:34 -0300

    koopa funcionando

---
+++ Assets/Scripts/Koopa.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class Koopa : MonoBehaviour
+{
+    public Sprite conchaSprite;
+    private bool encolhido = false;
+    private bool movendo = false;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(!encolhido && collision.gameObject.CompareTag("Player"))
+        {
+            if(collision.transform.DotTest(transform, Vector2.down))
+                Concha();
+            else
+                Destroy(collision.gameObject);//mario morre, trocar pra logica de reiniciar fase e tduo dps
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(encolhido && other.gameObject.CompareTag("Player")){
+            if(movendo){
+                Destroy(other.gameObject);
+            }else{
+                Vector2 direcao = new Vector2(transform.position.x - other.transform.position.x, 0f);
+                Empurra(direcao);
+            }
+        }
+    }
+
+    private void Concha()
+    {
+        GetComponent<PadraoInimigo>().enabled = false;
+        GetComponent<SpriteRenderer>().sprite = conchaSprite;
+        encolhido = true;
+    }
+
+    private void Empurra(Vector2 direcao)
+    {
+        movendo = true;
+
+        GetComponent<Rigidbody2D>().isKinematic = false;
+        PadraoInimigo movimento = GetComponent<PadraoInimigo>();
+        movimento.direcao = direcao.normalized;
+        movimento.velocidade = 12f;
+        movimento.enabled = true;
+    }
+}
