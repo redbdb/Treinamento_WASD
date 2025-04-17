7d1e3508b237545a61fbc78634958f5d50b6f18d
redbdb <gustavoasamaral@gmail.com>
2025-04-17 17:54:36 -0300

    animação de bloco pra cima funcionando

--- Assets/Scripts/Blocos.cs
+++ Assets/Scripts/Blocos.cs
@@ -1,7 +1,9 @@
+using System.Collections;
 using UnityEngine;
 
 public class Blocos : MonoBehaviour
 {   
+    public Sprite blocoVazio; 
     public int Maxhits = -1;
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -13,11 +15,37 @@ public class Blocos : MonoBehaviour
         }
     }
 
+    private IEnumerator Animacao(Vector3 de, Vector3 para)
+    {
+        Transform transform = GetComponent<Transform>();
+
+        float feito = 0f;
+        float duracao = 0.125f;
+
+        while(feito < duracao){
+            float t = feito/duracao;
+
+            transform.position = Vector3.Lerp(de, para, t);
+            feito += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.position = para;
+    }
+
     private void Hit()
     {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        Transform transform = GetComponent<Transform>();
+        Vector3 posI = transform.position;
+
+        StartCoroutine(Animacao(posI,  transform.position + Vector3.up * 0.5f));
+        StartCoroutine(Animacao(transform.position + Vector3.up * 0.5f, posI));
+
         Maxhits--;
         if(Maxhits == 0){
-
+            spriteRenderer.sprite = blocoVazio;
         }
     }
 }
